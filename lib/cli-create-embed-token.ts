e578c8fe529c4587c78c352cdff092468fb7eb56
anluong <anluong@microsoft.com>
2016-07-07 15:33:16 -0700

    Add support for generating RLS embed tokens

--- lib/cli-create-embed-token.ts
+++ lib/cli-create-embed-token.ts
@@ -7,16 +7,22 @@ import * as program from 'commander';
 export default function CliCreateEmbedToken() {
     let pkg = require('../package.json');
 
+    function list(val) {
+        return val.split(',');
+    }
+
     program.version(pkg.version)
         .option('-c, --collection <collection>', 'The Power BI workspace collection')
         .option('-k, --accessKey <accessKey>', 'The Power BI workspace collection access key')
-        .option('-w, --workspace [workspaceId]', 'The Power BI workspace id')
-        .option('-r, --report [reportId]', 'The Power BI report id');
+        .option('-w, --workspace <workspaceId>', 'The Power BI workspace id')
+        .option('-r, --report <reportId>', 'The Power BI report id')
+        .option('-u, --username [username]', 'The RLS username')
+        .option('--roles [roles]', 'The RLS roles', list);
 
     program.on('--help', function () {
         console.log('  Examples:');
         console.log('');
-        console.log('    $ powerbi create-embed-token -c <collection>, -w <workspace> -r <reportId> -k <accessKey>');
+        console.log('    $ powerbi create-embed-token -c <collection>, -w <workspace> -r <reportId> -k <accessKey> -u [username] --roles [roles1,roles2,...]');
     });
 
     program.parse(process.argv);
@@ -36,7 +42,7 @@ export default function CliCreateEmbedToken() {
             if (!(settings.collection && settings.workspace && settings.report)) {
                 return cli.error('collection, workspace and report params are required');
             }
-            token = powerbi.PowerBIToken.createReportEmbedToken(settings.collection, settings.workspace, settings.report);
+            token = powerbi.PowerBIToken.createReportEmbedToken(settings.collection, settings.workspace, settings.report, settings.username, settings.roles);
 
             let jwt = token.generate(settings.accessKey);
             cli.success('Embed Token: ', jwt);

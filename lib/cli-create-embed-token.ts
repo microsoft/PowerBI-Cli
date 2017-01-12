3f0e25bded8489cb90f7d831688d2c59fca709ee
ranbreuer <rbreuer@microsoft.com>
2017-01-12 14:34:48 +0200

    Rbreuer add new token support to CLI (#39)
    * Update README.md
    
    * Update README.md
    
    * add new token generation support
    
    * dep update
    
    * 1.0.5
    
    * update readme

--- lib/cli-create-embed-token.ts
+++ lib/cli-create-embed-token.ts
@@ -16,6 +16,8 @@ export default function CliCreateEmbedToken() {
         .option('-k, --accessKey <accessKey>', 'The Power BI workspace collection access key')
         .option('-w, --workspace <workspaceId>', 'The Power BI workspace id')
         .option('-r, --report <reportId>', 'The Power BI report id')
+        .option('-d, --dataset <datasetId>', 'The Power BI dataset id')
+        .option('-s, --scopes [scopes]', 'The permission scopes allowed')
         .option('-u, --username [username]', 'The RLS username')
         .option('--roles [roles]', 'The RLS roles', list);
 
@@ -23,6 +25,8 @@ export default function CliCreateEmbedToken() {
         console.log('  Examples:');
         console.log('');
         console.log('    $ powerbi create-embed-token -c <collection>, -w <workspace> -r <reportId> -k <accessKey> -u [username] --roles [roles1,roles2,...]');
+        console.log('    $ powerbi create-embed-token -c <collection>, -w <workspace> -d <datasetId> -k <accessKey>');
+        console.log('    $ powerbi create-embed-token -c <collection>, -w <workspace> -r <reportId> -k <accessKey> -s [scopes]');
     });
 
     program.parse(process.argv);
@@ -39,10 +43,14 @@ export default function CliCreateEmbedToken() {
                 return cli.error('Access key param is required');
             }
 
-            if (!(settings.collection && settings.workspace && settings.report)) {
-                return cli.error('collection, workspace and report params are required');
+            if (!(settings.collection && settings.workspace)) {
+                return cli.error('collection and workspace params are required');
             }
-            token = powerbi.PowerBIToken.createReportEmbedToken(settings.collection, settings.workspace, settings.report, settings.username, settings.roles);
+
+            if (!(settings.report || settings.dataset)) {
+                return cli.error('Either reportId or datasetId must be set');
+            }
+            token = powerbi.PowerBIToken.createReportEmbedToken(settings.collection, settings.workspace, settings.report, settings.dataset, settings.scopes, settings.username, settings.roles);
 
             let jwt = token.generate(settings.accessKey);
             cli.success('Embed Token: ', jwt);
